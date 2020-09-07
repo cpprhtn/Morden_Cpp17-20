c59112a4c546d1c4e589004c9dba5d6bdf1d490a
Lee_JunWon <cpprhtn@naver.com>
2020-09-07 22:16:58 +0900

    9/7

--- 1_Math/abundant number/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int x, y, z, k, sum, mok;
-    scanf("%d %d %d %d",&x,&y,&z,&k);
-    sum = (y/k) * (z/k);
-    mok = x > sum ? sum : x;
-    printf("%d",mok);
-}
