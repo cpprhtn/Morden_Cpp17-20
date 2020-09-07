2c5538cd95ce1c4c1acfde8b6390bdbc107ef6e6
Lee_JunWon <cpprhtn@naver.com>
2020-09-07 21:40:29 +0900

    9/7

---
+++ 1_Math/abundant number/abundant number.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cmath>
+//과잉수는 진약수들의 합이 수 자신보다 작은 수를 말함
+//12의 진약수는 1,2,3,4,6
+//이 수의 합은 16이므로 과잉수가 되며, 과잉은 16-12를 한 4가 됨
+int sum_proper_divisors(int const number)
+{
+   int result = 1;
+   for (int i = 2; i <= std::sqrt(number); i++)
+   {
+      if (number%i == 0)
+      {
+         result += (i == (number / i)) ? i : (i + number / i);
+      }
+   }
+
+   return result;
+}
+
+void print_abundant(int const limit)
+{
+   for (int number = 10; number <= limit; ++number)
+   {
+      auto sum = sum_proper_divisors(number);
+      if (sum > number)
+      {
+         std::cout
+            << number
+            << ", abundance=" << sum - number << std::endl;
+      }
+   }
+}
+
+int main()
+{
+   int limit = 0;
+   std::cout << "Upper limit: ";
+   std::cin >> limit;
+
+   print_abundant(limit);
+}
