d2c6403a96aa84565e601b8607520995f35b2b5e
Lee_JunWon <cpprhtn@naver.com>
2020-09-10 23:43:35 +0900

    9/10

---
+++ 1_Math/gray code/gray code.cpp
@@ -0,0 +1,41 @@
+//gray code는 연속된 두 수가 오직 한 비트 차이가 나도록 만드는 이진 인코딩 방법
+
+
+#include <iostream>
+#include <bitset>
+#include <string>
+
+unsigned int gray_encode(unsigned int const num)
+{
+   return num ^ (num >> 1);
+}
+
+unsigned int gray_decode(unsigned int gray)
+{
+   for (unsigned int bit = 1U << 31; bit > 1; bit >>= 1)
+   {
+      if (gray & bit) gray ^= bit >> 1;
+   }
+   return gray;
+}
+
+std::string to_binary(unsigned int value, int const digits)
+{
+   return std::bitset<32>(value).to_string().substr(32-digits, digits);
+}
+
+int main()
+{
+   std::cout << "Number\tBinary\tGray\tDecoded\n";
+   std::cout << "------\t------\t----\t-------\n";
+
+   for (unsigned int n = 0; n < 32; ++n)
+   {
+      auto encg = gray_encode(n);
+      auto decg = gray_decode(encg);
+
+      std::cout 
+         << n << "\t" << to_binary(n, 5) << "\t" 
+         << to_binary(encg, 5) << "\t" << decg << "\n";
+   }
+}
