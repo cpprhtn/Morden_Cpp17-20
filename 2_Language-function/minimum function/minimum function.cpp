f3717b17bd85bf8f8bdf95e5009fd8f5db09cf20
Lee_JunWon <cpprhtn@naver.com>
2020-09-14 20:33:05 +0900

    9/14

---
+++ 2_Language-function/minimum function/minimum function.cpp
@@ -0,0 +1,32 @@
+//여러 수를 인자로 받는 최소 함수
+//여러 개의 수를 전달 인자로 받고, < 연산자를 사용해 인자들을 비교한 뒤 가장 작은값을 반환하는 함수 템플릿을 작성하라
+
+#include <iostream>
+#include <functional>
+
+template <typename T>
+T minimum(T const a, T const b) { return a < b ? a : b; }
+
+template <typename T1, typename... T>
+T1 minimum(T1 a, T... args)
+{
+    return minimum(a, minimum(args...));
+}
+
+template <class Compare, typename T>
+T minimumc(Compare comp, T const a, T const b) { return comp(a, b) ? a : b; }
+
+template <class Compare, typename T1, typename... T>
+T1 minimumc(Compare comp, T1 a, T... args)
+{
+    return minimumc(comp, a, minimumc(comp, args...));
+}
+
+int main()
+{
+    auto x = minimum(5, 4, 2, 3);
+    std::cout << x << std::endl;
+
+    auto y = minimumc(std::less<>(), 3, 2, 1, 0);
+    std::cout << y << std::endl;
+}
